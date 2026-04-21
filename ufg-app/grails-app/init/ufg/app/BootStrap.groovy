d4fbdd2c0e286e3ed1432f849c734eaa7405b788
lanzewinfo <tim.lanzendoerfer@iu-study.org>
2026-04-21 13:27:40 +0200

    fix: Added initial users via BootStrap config

--- ufg-app/grails-app/init/ufg/app/BootStrap.groovy
+++ ufg-app/grails-app/init/ufg/app/BootStrap.groovy
@@ -3,6 +3,13 @@ package ufg.app
 class BootStrap {
 
     def init = {
+        if (!User.findByUsername("testuser")) {
+            new User(username: "testuser", password: "testpass", role: "USER").save(failOnError: true)
+        }
+
+        if (!User.findByUsername("admin")) {
+            new User(username: "admin", password: "adminpass", role: "ADMIN").save(failOnError: true)
+        }
     }
 
     def destroy = {
